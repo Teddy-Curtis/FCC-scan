b5628ce2dc28e8803da04b6d01b909397500436d
ecurtis <edward.curtis@cern.ch>
2024-08-19 11:17:17 +0100

    Added the correct ffspec install and removed the copying of the files.  Please enter the commit message for your changes. Lines starting

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pip install mplhep
+pip install fsspec-xrootd
 conda install pytorch==1.12.1 cudatoolkit=10.2 -c pytorch -c nvidia
 
-scp -r ecurtis@lxplus.cern.ch://eos/user/a/amagnan/FCC/iDMprod/winter2023/stage3 data
